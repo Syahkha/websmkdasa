4f7ad2451dad277f951918d2e6fd713628e46f74
Syahkha <49738236+Syahkha@users.noreply.github.com>
2020-01-10 14:05:19 +0700

    start

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,88 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login V10</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="admin/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="admin/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="admin/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="admin/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="admin/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="admin/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="admin/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="admin/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="admin/css/util.css">
+	<link rel="stylesheet" type="text/css" href="admin/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-t-50 p-b-90">
+			<form method="POST" action="{{ route('login') }}">
+                        @csrf
+					<span class="login100-form-title p-b-51">
+						Login
+					</span>
+
+					
+					<div class="wrap-input100 validate-input m-b-16" data-validate = "Username is required">
+						<input class="input100"  id="email" type="email" name="email" placeholder="Username" autocomplete="off" required>
+						<span class="focus-input100"></span>
+					</div>
+					
+					
+					<div class="wrap-input100 validate-input m-b-16" data-validate = "Password is required">
+						<input class="input100" id="password" type="password" name="password" placeholder="Password" required>
+						<span class="focus-input100"></span>
+					</div>
+					
+				
+
+					<div class="container-login100-form-btn m-t-17">
+						<button type="submit" class="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="admin/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="admin/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="admin/vendor/bootstrap/js/popper.js"></script>
+	<script src="admin/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="admin/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="admin/vendor/daterangepicker/moment.min.js"></script>
+	<script src="admin/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="admin/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="admin/js/main.js"></script>
+
+</body>
+</html>
