d4a61855877d754f79c457166760630cc6bd1532
ajiputraprayogi <49738174+ajiputraprayogi@users.noreply.github.com>
2020-01-11 08:28:09 +0700

    pecah template
    pecah template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,6 @@
 
 <div class="container-fluid">
 
-<!-- Breadcrumbs-->
-<ol class="breadcrumb">
-  <li class="breadcrumb-item">
-    <a href="#">Dashboard</a>
-  </li>
-</ol>
-
 <!-- Icon Cards-->
 <div class="row">
   <div class="col-xl-3 col-sm-6 mb-3">
