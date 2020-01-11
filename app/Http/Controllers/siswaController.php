c91183b5ef2fa37d8e06b442d7b21dcc3a5930b0
ALFIAN <icaca2002@gmail.com>
2020-01-11 11:25:28 +0700

    Create siswaController.php

---
+++ app/Http/Controllers/siswaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class siswaController extends Controller
+{
+    public function siswa(){
+        return view('siswa_controller');
+    }
+}
