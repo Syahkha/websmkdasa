49970774301a68363e55b634d82a2486a313609e
ajiputraprayogi <49738174+ajiputraprayogi@users.noreply.github.com>
2020-01-11 08:47:16 +0700

    po

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -10,7 +10,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SMK PLUS DARUSSALAM</title>
+  <title>SMK PLUS DARUSSALAM @title</title>
 
   <!-- Custom fonts for this template-->
   <link href="{{asset('user/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
