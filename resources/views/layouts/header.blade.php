512dfd0dcabf8509ee12f6cb2343d2e4e00a1bd0
Syahkha <49738236+Syahkha@users.noreply.github.com>
2020-01-18 11:01:20 +0700

    kemem

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,8 +11,8 @@
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fa fa-user"></i> 
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->name}}</span>                         
+              <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->name}}</span> 
+              <i class="fa fa-user"></i>                                       
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
