a57a3bb443b99b9fc1cfe038b30ecd9debd9496a
Rizkita Fandy <59752119+llenn27@users.noreply.github.com>
2020-02-13 22:35:03 +0700

    Last!
    FE Artikel Fix

--- resources/views/front/user/detail_artikel.blade.php
+++ resources/views/front/user/detail_artikel.blade.php
@@ -3,14 +3,18 @@
 Blog
 @endsection
 @section('konten')
-    <div class="blog-area mt-50 section-padding-100">
+@foreach($setting as $data)
+<div class="breadcumb-area bg-img" style="background-image: url({{asset('source/banner/'.$data->banner)}});">
+    @endforeach
+</div>
+    <div class="blog-area mt-50">
         <div class="container">
             <div class="row">
                 <div class="col-12 col-md-12">
                     <div class="academy-blog-posts">
                         <div class="row">
                             @foreach($artikel as $data)
-                            <div class="blog">
+                            <div class="single-blog-post">
                                 <div class="blog-img">
                                     <img class="img-responsive" src="{{asset('source/artikel/'.$data->gambar)}}" alt="">
                                 </div>
@@ -24,11 +28,11 @@ Blog
                                     <h3>{{$data->judul}}</h3>
                                     {!!$data->artikel!!}
                                 </div>
+                                <a href="{{url('tampilblog')}}" class="btn academy-btn btn-sm mt-15">Kembali</a>
                             </div>
                             @endforeach
-
                         </div>
-                        <a href="{{url('tampilblog')}}" class="btn academy-btn btn-sm mt-15">Kembali</a>
+                        
                     </div><br>
             </div>
         </div>
